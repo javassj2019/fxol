b2da0e58be54069dc575f19cc6ac84f8ecaff53b
javassj <javassj@qq.com>
2021-08-24 20:51:52 +0800

    修改了学习过程的容错机制

--- fxol.py
+++ fxol.py
@@ -52,7 +52,7 @@ headers['Connection'] = 'keep-alive'
 headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
 answerjson = '\[(.*)\]'
 
-####比较最后更新时间并写数据库
+####获取当前日期
 today = str(datetime.date.today())
 
 
@@ -63,112 +63,95 @@ l = cur.execute('select UserID,UserPassword from User where Mark = (%s)', (2))
 while s <= l:
     cur.execute('select UserID,UserPassword from User where Mark = (%s)', (2))
     tt1 = cur.fetchone()
-    print(tt1)
-    userAccount = tt1['UserID']
-    userPassword = tt1['UserPassword']
-    # 登陆并获取参数
-    tt = requests.get(http + host + loginurl + 'userAccount=' + userAccount + '&userPassword=' + userPassword + code,
-                      headers=headers)
-    dd = json.loads(tt.text)
-    sid = dd['data']['sid']
-    # username = dd['data']['userName']
-    # 获取学员基础信息
-    t2 = requests.get(http + host + getdetailurl + userAccount + '&ssid=' + sid, headers=headers)
-    t2.encoding = 'utf8'
-    d2 = json.loads(t2.text)
-    lenth = len(d2)
-    dict1 = {}
-    i = 0
-    while i <= lenth-1:
-        dict1[d2[i]] = d2[i + 1]
-        i = i + 2
-    TDpoint = dict1['todaytpoint']
-    domainCode = dict1['domainCode']
-    Tpoint = dict1['tpoint']
-    username = dict1['userName']
-    print(username)
-    # 开始学习,下一行是学习时间提交的
-    t3 = requests.get(
-        http + host + studyurl + domainCode + '&userAccount=' + userAccount + '&stime=241&ssid=' + sid + '&validate=&type=2',
-        headers=headers)
-    # 开始自动做练习题
-    # t4 = requests.get(
-    #     http + host + '/ess/service/getpaper?paperId=2790&series=21_answer&version=2.5.5&userAccount' + userAccount,
-    #     headers=headers)
-    # t4answer = re.findall(answerjson, str(t4.content))[0]  # 此处返回text会因为返回有非json的数据不能读取
-    # # print(type(t4answer),t4answer)
-    # t5 = json.loads('[' + t4answer + ']')
-    # # print(t5[0])
-    # k = 0
-    # answer = ''
-    # while k <= 49:
-    #     answer = answer + '{%22questionId%22:' + t5[k]['questionId'] + ',%22answerNo%22:%22' + t5[k][
-    #         'answerNo'] + '%22}'
-    #     k = k + 1
-    # t6 = requests.get(
-    #     http + host + '/sss/service/coursewareService!commitExercises.do?domainCode=' + domainCode + '&userAccount=' + userAccount + '&paperId=2790&series=21&myExamAnswer=[' + answer + ']&ssid=' + sid + '&validate=&type=2',
-    #     headers=headers)
-    # tkk = requests.post('http://mobile.faxuan.net/ess/service/myexam/myExamAo!doCommitExam.do',headers=headers,data='domainCode='+domainCode+'&series=43&userAccount='+userAccount+'&examId=2034&myExamAnswer=%5b%7b%22questionId%22%3a%22566846%22%22answerNo%22%3a%22A%22%7d%7b%22questionId%22%3a%22566833%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566851%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566840%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566828%22%22answerNo%22%3a%22D%22%7d%7b%22questionId%22%3a%22566842%22%22answerNo%22%3a%22A%22%7d%7b%22questionId%22%3a%22566830%22%22answerNo%22%3a%22A%22%7d%7b%22questionId%22%3a%22566838%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566836%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566829%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566834%22%22answerNo%22%3a%22D%22%7d%7b%22questionId%22%3a%22566854%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566843%22%22answerNo%22%3a%22D%22%7d%7b%22questionId%22%3a%22566848%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566835%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566827%22%22answerNo%22%3a%22D%22%7d%7b%22questionId%22%3a%22566832%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566831%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566852%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566844%22%22answerNo%22%3a%22D%22%7d%7b%22questionId%22%3a%22566826%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566847%22%22answerNo%22%3a%22A%22%7d%7b%22questionId%22%3a%22566839%22%22answerNo%22%3a%22A%22%7d%7b%22questionId%22%3a%22566837%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566841%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566853%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566850%22%22answerNo%22%3a%22A%22%7d%7b%22questionId%22%3a%22566855%22%22answerNo%22%3a%22B%22%7d%7b%22questionId%22%3a%22566845%22%22answerNo%22%3a%22D%22%7d%7b%22questionId%22%3a%22566849%22%22answerNo%22%3a%22C%22%7d%7b%22questionId%22%3a%22566934%22%22answerNo%22%3a%22ABC%22%7d%7b%22questionId%22%3a%22566916%22%22answerNo%22%3a%22ABD%22%7d%7b%22questionId%22%3a%22566925%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566921%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566922%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566928%22%22answerNo%22%3a%22BC%22%7d%7b%22questionId%22%3a%22566920%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566930%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566918%22%22answerNo%22%3a%22AB%22%7d%7b%22questionId%22%3a%22566924%22%22answerNo%22%3a%22AB%22%7d%7b%22questionId%22%3a%22566919%22%22answerNo%22%3a%22ABC%22%7d%7b%22questionId%22%3a%22566931%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566923%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566933%22%22answerNo%22%3a%22BCD%22%7d%7b%22questionId%22%3a%22566932%22%22answerNo%22%3a%22ABC%22%7d%7b%22questionId%22%3a%22566927%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566926%22%22answerNo%22%3a%22ABC%22%7d%7b%22questionId%22%3a%22566935%22%22answerNo%22%3a%22BC%22%7d%7b%22questionId%22%3a%22566917%22%22answerNo%22%3a%22ABC%22%7d%7b%22questionId%22%3a%22566929%22%22answerNo%22%3a%22ABCD%22%7d%7b%22questionId%22%3a%22566977%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566970%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566968%22%22answerNo%22%3a%220%22%7d%7b%22questionId%22%3a%22566974%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566978%22%22answerNo%22%3a%220%22%7d%7b%22questionId%22%3a%22566971%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566964%22%22answerNo%22%3a%220%22%7d%7b%22questionId%22%3a%22566975%22%22answerNo%22%3a%220%22%7d%7b%22questionId%22%3a%22566976%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566969%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566966%22%22answerNo%22%3a%220%22%7d%7b%22questionId%22%3a%22566967%22%22answerNo%22%3a%220%22%7d%7b%22questionId%22%3a%22566973%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566965%22%22answerNo%22%3a%221%22%7d%7b%22questionId%22%3a%22566972%22%22answerNo%22%3a%221%22%7d%5d&paperId=4409&')
-    # #print(tkk.text)
-    # 开始做练习
-    #第一次练习
-    paperid = str(4978)
-    series = str(29)
-    t4 = requests.get(
-        http + host + '/ess/service/getpaper?paperId='+paperid+'&series='+series+'_answer&version=2.7.6&userAccount' + userAccount,
-        headers=headers)
-    # print(t4.text)
-    t4answer = re.findall(answerjson, str(t4.content))[0]  # 此处返回text会因为返回有非json的数据不能读取
+    try:
+        print(tt1)
+        userAccount = tt1['UserID']
+        userPassword = tt1['UserPassword']
+        # 登陆并获取参数
+        tt = requests.get(http + host + loginurl + 'userAccount=' + userAccount + '&userPassword=' + userPassword + code,headers=headers)
+        dd = json.loads(tt.text)
+        sid = dd['data']['sid']
+        # username = dd['data']['userName']
+        # 获取学员基础信息
+        t2 = requests.get(http + host + getdetailurl + userAccount + '&ssid=' + sid, headers=headers)
+        t2.encoding = 'utf8'
+        d2 = json.loads(t2.text)
+        lenth = len(d2)
+        dict1 = {}
+        i = 0
+        while i <= lenth-1:
+            dict1[d2[i]] = d2[i + 1]
+            i = i + 2
+        TDpoint = dict1['todaytpoint']
+        domainCode = dict1['domainCode']
+        Tpoint = dict1['tpoint']
+        username = dict1['userName']
+        print(username)
+        # 开始学习,下一行是学习时间提交的
+        t3 = requests.get(
+            http + host + studyurl + domainCode + '&userAccount=' + userAccount + '&stime=241&ssid=' + sid + '&validate=&type=2',
+            headers=headers)
+        # 开始自动做练习题
+        #第一次练习
+        paperid = str(4978)
+        series = str(29)
+        t4 = requests.get(
+            http + host + '/ess/service/getpaper?paperId='+paperid+'&series='+series+'_answer&version=2.7.6&userAccount' + userAccount,
+            headers=headers)
+        # print(t4.text)
+        t4answer = re.findall(answerjson, str(t4.content))[0]  # 此处返回text会因为返回有非json的数据不能读取
+        t4answer = t4answer.replace(',"score":"10.0",', ',')
+        answer = t4answer
+        data = {
+            'series': series,
+            'paperId': paperid,
+            'userAccount': userAccount,
+            'domainCode': domainCode,
+            'ssid': sid,
+            'type': '2',
+            'myExamAnswer': '[' + answer + ']'
+        }
+        t6 = requests.get(
+            http + host + '/sss/service/coursewareService!commitExercises.do',
+            headers=headers,
+            params=data
+        )
+        print(t6.text)
+        #第二次练习
+        paperid = str(4978)
+        series = str(29)
+        t4 = requests.get(
+            http + host + '/ess/service/getpaper?paperId='+paperid+'&series='+series+'_answer&version=2.7.6&userAccount' + userAccount,
+            headers=headers)
+        # print(t4.text)
+        t4answer = re.findall(answerjson, str(t4.content))[0]  # 此处返回text会因为返回有非json的数据不能读取
 
-    t4answer = t4answer.replace(',"score":"10.0",', ',')
-    answer = t4answer
-    data = {
-        'series': series,
-        'paperId': paperid,
-        'userAccount': userAccount,
-        'domainCode': domainCode,
-        'ssid': sid,
-        'type': '2',
-        'myExamAnswer': '[' + answer + ']'
-    }
-    t6 = requests.get(
-        http + host + '/sss/service/coursewareService!commitExercises.do',
-        headers=headers,
-        params=data
-    )
-    print(t6.text)
-    #第二次练习
-    paperid = str(4978)
-    series = str(29)
-    t4 = requests.get(
-        http + host + '/ess/service/getpaper?paperId='+paperid+'&series='+series+'_answer&version=2.7.6&userAccount' + userAccount,
-        headers=headers)
-    # print(t4.text)
-    t4answer = re.findall(answerjson, str(t4.content))[0]  # 此处返回text会因为返回有非json的数据不能读取
+        t4answer = t4answer.replace(',"score":"10.0",', ',')
+        answer = t4answer
+        data = {
+            'series': series,
+            'paperId': paperid,
+            'userAccount': userAccount,
+            'domainCode': domainCode,
+            'ssid': sid,
+            'type': '2',
+            'myExamAnswer': '[' + answer + ']'
+        }
+        t6 = requests.get(
+            http + host + '/sss/service/coursewareService!commitExercises.do',
+            headers=headers,
+            params=data
+        )
+        print(t6.text)
 
-    t4answer = t4answer.replace(',"score":"10.0",', ',')
-    answer = t4answer
-    data = {
-        'series': series,
-        'paperId': paperid,
-        'userAccount': userAccount,
-        'domainCode': domainCode,
-        'ssid': sid,
-        'type': '2',
-        'myExamAnswer': '[' + answer + ']'
-    }
-    t6 = requests.get(
-        http + host + '/sss/service/coursewareService!commitExercises.do',
-        headers=headers,
-        params=data
-    )
-    print(t6.text)
-
-    cur.execute('UPDATE User SET Name = (%s) WHERE UserID = (%s)', (username, userAccount))
-    cur.execute('UPDATE User SET Tpoint = (%s) WHERE UserID = (%s)', (Tpoint, userAccount))
-    cur.execute('UPDATE User SET TDpoint = (%s) WHERE UserID = (%s)', (TDpoint, userAccount))
-    cur.execute('UPDATE User SET Updata = (%s) WHERE UserID = (%s)', (today, userAccount))
-    cur.execute('UPDATE User SET Mark = (%s) WHERE UserID = (%s)', (0, userAccount))
-    conn.commit()
-    print("学习过程结束")
-    ##########这里还差学习执行过程
+        cur.execute('UPDATE User SET Name = (%s) WHERE UserID = (%s)', (username, userAccount))
+        cur.execute('UPDATE User SET Tpoint = (%s) WHERE UserID = (%s)', (Tpoint, userAccount))
+        cur.execute('UPDATE User SET TDpoint = (%s) WHERE UserID = (%s)', (TDpoint, userAccount))
+        cur.execute('UPDATE User SET Updata = (%s) WHERE UserID = (%s)', (today, userAccount))
+        cur.execute('UPDATE User SET Mark = (%s) WHERE UserID = (%s)', (0, userAccount))
+        conn.commit()
+        print("学习过程结束")
+    except TypeError:
+        print('未找到符合条件的数据，请检查数据库')
+        conn.close()
+        break
